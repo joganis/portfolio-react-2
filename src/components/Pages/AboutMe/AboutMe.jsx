210de6102487b964a986032e068f6befeba14117
joganis <joganis@hotmail.com>
2024-10-30 18:54:09 -0500

    se organiza la imagen

--- src/components/Pages/AboutMe/AboutMe.jsx
+++ src/components/Pages/AboutMe/AboutMe.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import img3 from '../../../assets/yoojee.png';
 export const AboutMe = () => {
   return (
     <section id="about">
-      <img src="src\assets\yoojee.png" alt="Foto de perfil" />
+      <img src={img3} alt="Foto de perfil" />
       <div>
       <h2>Sobre Mí</h2>
       <p> ¡Hola! Mi nombre es Jobany y disfruto desarrollar diversas aplicaciones, como APIs y páginas web, así como consumir APIs y más. Mi interés en el desarrollo web comenzó en 2022, cuando decidí iniciar mis estudios profesionales como Ingeniero de Sistemas. A partir de ese momento, me sumergí profundamente en la programación; aprender lógica de programación con Python y Java me permitió afianzar conceptos y avanzar en temas relacionados con el desarrollo de software. </p>
